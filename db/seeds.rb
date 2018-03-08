e9980f6ad8bdf6cd2fd1aed5d0b9e3c77db68b71
Gautam Moradiya <vaishali-bacancy>
2018-03-08 14:40:38 +0530

    added seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+ROLES=["admin","user"]    
+ROLES.each do |role|     
+	Role.find_or_create_by(name: role)   
+end   
+admin= User.find_by(email:"vaishali.pitroda@bacancytechnology.com") 
+unless admin.present?  
+	admin = User.create(username:"vaishali",email:"vaishali.pitroda@bacancytechnology.com")   
+	admin.password =  "123456"      
+	admin.save               
+end   
+admin.add_role "admin"  
+puts admin.errors.full_messages
